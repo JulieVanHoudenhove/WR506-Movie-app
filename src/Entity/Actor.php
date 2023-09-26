0f9359ce6f230e2a6095546badd64d6ce0e5717c
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-26 08:58:47 +0200

    feat : add nationality & fixtures

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -28,6 +28,9 @@ class Actor
     #[ORM\ManyToMany(targetEntity: Movie::class, mappedBy: 'actor')]
     private Collection $movies;
 
+    #[ORM\ManyToOne(inversedBy: 'actors')]
+    private ?Nationality $nationality = null;
+
     public function __construct()
     {
         $this->movies = new ArrayCollection();
@@ -85,6 +88,18 @@ class Actor
         if ($this->movies->removeElement($movie)) {
             $movie->removeActor($this);
         }
+
+        return $this;
+    }
+
+    public function getNationality(): ?Nationality
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(?Nationality $nationality): static
+    {
+        $this->nationality = $nationality;
 
         return $this;
     }
