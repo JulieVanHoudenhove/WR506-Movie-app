88b9fd17aae7d707f543f1479b957169872e8718
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2023-09-28 11:55:27 +0200

    feat : create security and user with his fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+    public function __construct(protected UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        foreach (range(1, 5) as $i) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@gmail.com');
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password' . $i));
+            $this->addReference('user_' . $i, $user);
+            $manager->persist($user);
+        }
+
+        $admin = new User();
+        $admin->setEmail('admin@gmail.com');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $this->addReference('admin_', $admin);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
