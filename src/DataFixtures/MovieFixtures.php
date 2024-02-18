fa9c685a1a2316857d9f28ddf96d53fd99fa68a6
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-02-18 14:32:47 +0100

    feat: change movie fixtures by faker

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -6,39 +6,39 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Movie;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker\Factory;
 
 class MovieFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
 
         foreach (range(1, 40) as $i) {
-            $movie = new Movie();
-            $movie->setTitle('Movie ' . $i);
-            $movie->setReleaseDate(new \DateTime());
-            $movie->setDuration(rand(60, 180));
-            $movie->setDescription('Synopsis ' . $i);
-            $movie->setDirector('director' . rand(1, 10));
-            $movie->setCategory($this->getReference('category_' . rand(1, 5)));
-            $movie->setOnline((bool) rand(0, 1));
-            $actors = [];
-            foreach (range(1, rand(1, 5)) as $j) {
-                $actor = $this->getReference('actor_' . rand(1, 10));
-                if (!in_array($actor, $actors)) {
-                    $actors[] = $actor;
-                    $movie->addActor($actor);
+            $faker = Factory::create('fr_FR');
+            $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+
+            foreach (range(1, 40) as $i) {
+                $movie = new Movie();
+                $movie->setTitle($faker->unique()->movie);
+                $movie->setDescription('Synopsis ' . $i);
+                $movie->setDuration(rand(60, 180));
+                $movie->setOnline((bool) rand(0, 1));
+                $movie->setReleaseDate($faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null));
+                $movie->setCategory($this->getReference('category_' . rand(1, 5)));
+                $movie->setDirector('Director' . rand(1, 5));
+                foreach (range(1, rand(1, 5)) as $j) {
+                    $movie->addActor($this->getReference('actor_' . rand(1, 20)));
                 }
+                $manager->persist($movie);
             }
-            $manager->persist($movie);
-//            $this->addReference('movie_' . $i, $movie);
         }
 
         $manager->flush();
     }
 
-    public function getDependencies()
+    public function getDependencies(): array
     {
         return [
             CategoryFixtures::class,
