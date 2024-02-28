cedb0574dcf292f179228859ec9058f7b439e1b3
Julie Van Houdenhove <100696436+JulieVanHoudenhove@users.noreply.github.com>
2024-02-28 22:32:08 +0100

    feat: wip - upload media

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -15,12 +15,23 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
         $faker = Factory::create('fr_FR');
         $faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
 
+        $rewards = [
+            'Oscar',
+            'Golden Globe',
+            'BAFTA',
+            'Emmy',
+            'César',
+            'Palme d\'or',
+            'Lion d\'or',
+            'Prix d\'interprétation'
+        ];
+
         foreach (range(1, 20) as $i) {
             $actor = new Actor();
             $fullname = $faker->unique()->actor;
             $actor->setfirstName(substr($fullname, 0, strpos($fullname, ' ')));
             $actor->setlastName(substr($fullname, strpos($fullname, ' ') + 1));
-//            $actor->setreward($rewards[rand(0, 9)]);
+            $actor->setreward($rewards[rand(0, 7)]);
             $actor->setNationality($this->getReference('nationality_' . rand(1, 10)));
             $manager->persist($actor);
             $this->addReference('actor_' . $i, $actor);
